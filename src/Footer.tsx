fb5409dc6f6295f08cbd0ce6a03309ed25c4e659
ProfL3301 <ProfL3301@lab.aca>
2025-01-23 11:15:50 -0500

    Cambio de color de footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,10 +1,27 @@
+import { useState } from "react"
 import "./Footer.css"
 
 const Footer = () => {
-    const estilo = {
-        backgroundColor : "red"
+    const [color, setColor] = useState<number>(0)
+
+    const posiblesColores = [
+        "red", "blue", "peru"
+    ]
+
+    const footerOnClick = () => {
+        if (posiblesColores.length > 0)
+        {
+            const pos = Math.floor(Math.random() * (posiblesColores.length))
+            setColor(pos)
+        }
     }
-    return <div className="card mt-2" style={ estilo }>
+
+    const estilo = {
+        backgroundColor : posiblesColores[color]
+    }
+    return <div className="card mt-2" 
+                style={ estilo } 
+                onClick={ footerOnClick }>
         <div className="card-body texto-centrado texto-blanco">
             Programación Web :: 2025-0
         </div>
