ace2a6a3c2f163adb47574a379ecdcfed4ef638e
ProfL3301 <ProfL3301@lab.aca>
2025-01-23 12:23:00 -0500

    Mejoras en agregar TODO

--- src/FormularioTODO.tsx
+++ src/FormularioTODO.tsx
@@ -11,18 +11,28 @@ const FormularioTODO = (props : FormularioTODOProps) => {
         setDescripcionTODO(e.currentTarget.value)
     }
 
+    const descripcionTODOOnKeyDown = (e : React.KeyboardEvent<HTMLInputElement >) => {
+        //console.log(e.key)
+        if (e.key == "Enter") {
+            props.onAgregarTodo(descripcionTODO)
+            setDescripcionTODO("")
+        }
+    }
+
     return <section className="row">
         <div className="col-md-11">
             <input className="form-control" 
                     type="text" 
                     value={ descripcionTODO }
-                    onChange={ descripcionTODOOnChange }/>
+                    onChange={ descripcionTODOOnChange }
+                    onKeyDown={ descripcionTODOOnKeyDown }/>
         </div>
         <div className="col-md-1">
             <button className="btn btn-primary" 
                 type="button"
                 onClick={ () => {
                     props.onAgregarTodo(descripcionTODO)
+                    setDescripcionTODO("")
                 } }>
                 +
             </button>
