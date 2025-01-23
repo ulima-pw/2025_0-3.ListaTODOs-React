4a0f70f4e40e17fe56f7c937bac3517c7d39c98e
ProfL3301 <ProfL3301@lab.aca>
2025-01-23 11:49:33 -0500

    Manejando elementos input de form

--- src/FormularioTODO.tsx
+++ src/FormularioTODO.tsx
@@ -1,7 +1,18 @@
+import { useState } from "react"
+
 const FormularioTODO = () => {
+    const [descripcionTODO, setDescripcionTODO] = useState<string>("")
+
+    const descripcionTODOOnChange = (e : React.ChangeEvent<HTMLInputElement >) => {
+        setDescripcionTODO(e.currentTarget.value)
+    }
+
     return <section className="row">
         <div className="col-md-11">
-            <input className="form-control" type="text" value=""/>
+            <input className="form-control" 
+                    type="text" 
+                    value={ descripcionTODO }
+                    onChange={ descripcionTODOOnChange }/>
         </div>
         <div className="col-md-1">
             <button className="btn btn-primary" type="button">+</button>
